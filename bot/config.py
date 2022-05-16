a54e23d59cd4ae4dd21e5c96ff48df3a95ddc3d3
sidd-2005 <93574788+sidd-2005@users.noreply.github.com>
2022-05-16 11:52:07 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -23,7 +23,7 @@ try:
     OWNER = config("OWNER")
     FFMPEG = config(
         "FFMPEG",
-        default='ffmpeg -i "{}" -ss 00:00:59 -frames:v 1 "{}"',
+        default='ffmpeg -i "{}" -ss 00:00:59 "{}"',
     )
     THUMB = config(
         "THUMBNAIL", default="www.google.com"

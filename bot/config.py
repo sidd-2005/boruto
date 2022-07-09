6893e2883ef4be7de5b46a92b1b0f3c05330fe03
Grim-Reaper-San <103260040+Grim-Reaper-San@users.noreply.github.com>
2022-07-09 22:53:23 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -23,7 +23,7 @@ try:
     OWNER = config("OWNER")
     FFMPEG = config(
         "FFMPEG",
-                default='''ffmpeg -i "{}" -filter_complex "[0:v]drawtext=fontfile=font.ttf:text='t.me/animext':fontsize=18:fontcolor=ffffff:alpha='if(lt(t,0),0,if(lt(t,5),(t-0)/5,if(lt(t,15),1,if(lt(t,20),(5-(t-15))/5,0))))':x=w-text_w-15:y=15" -pix_fmt yuv420p -s 1440x900 -preset veryfast -c:v h264 -crf 24.2 -map 0:v -c:a aac -b:a 128k -map 0:a -c:s copy -map 0:s? "{}"''',
+                default='''ffmpeg -i "{}" -filter_complex "[0:v]drawtext=fontfile=font.ttf:text='t.me/animext':fontsize=18:fontcolor=ffffff:alpha='if(lt(t,0),0,if(lt(t,5),(t-0)/5,if(lt(t,15),1,if(lt(t,20),(5-(t-15))/5,0))))':x=w-text_w-15:y=15" -pix_fmt yuv420p10le -s 1440x900 -preset fast -c:v h264 -crf 24.2 -map 0:v -c:a aac -b:a 128k -map 0:a -c:s copy -map 0:s? "{}"''',
     )
     THUMB = config(
         "THUMBNAIL", default="www.google.com"
